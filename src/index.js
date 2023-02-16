60f0a1efaa8edac6beafd4dff138f097c5b1502a
Latira <l@g.com>
2023-02-17 00:07:29 +0300

    feat: progress version

--- src/index.js
+++ src/index.js
@@ -1,71 +1,93 @@
 module.exports = function toReadable (number) {
-    // let num_arr = [];
-  let readable = [];
-  let str = String(number).length;
-  if (str==1) {
-    readable = numToString(number);
-  } else if ((number % 10) == 0 || (Math.floor(number/10) == 1)) {
-    readable = numToString(number);
-  } return readable;
-}
+    let readable = '';
+    let str = String(number).length;
+    if (str==1 || (str==2 && (number % 10) == 0) || (str==2 && Math.floor(number/10) == 1)) {
+      readable = numToString(number);
+    } else {
+      for (i=0; i<str; i++) {
+        readable = numToString((number % 10)*(10**(i))) + ' ' + readable; 
+        console.log(readable);
+        number = Math.floor(number / 10);
+      }
+    } 
+    return readable;
+  }
 
-function numToString(num) {
-  let stringNum;
-  if (num==1) {
-    stringNum = 'one';
-  } else if (num==2) {
-    stringNum = 'two';
-  } else if (num==3) {
-    stringNum = 'three';
-  } else if (num==4) {
-    stringNum = 'four';
-  } else if (num==5) {
-    stringNum = 'five';
-  } else if (num==6) {
-    stringNum = 'six';
-  } else if (num==7) {
-    stringNum = 'seven';
-  } else if (num==8) {
-    stringNum = 'eight';
-  } else if (num==9) {
-    stringNum = 'nine';
-  } else if (num==0) {
-    stringNum = 'zero';
-  } else if (num==10) {
-    stringNum = 'ten';
-  } else if (num==11) {
-    stringNum = 'eleven';
-  } else if (num==12) {
-    stringNum = 'twelve';
-  } else if (num==13) {
-    stringNum = 'thirteen';
-  } else if (num==14) {
-    stringNum = 'fourteen';
-  } else if (num==15) {
-    stringNum = 'fifteen';
-  } else if (num==16) {
-    stringNum = 'sixteen';
-  } else if (num==17) {
-    stringNum = 'seventeen';
-  } else if (num==18) {
-    stringNum = 'eighteen';
-  } else if (num==19) {
-    stringNum = 'nineteen';
-  } else if (num==20) {
-    stringNum = 'twenty';
-  } else if (num==30) {
-    stringNum = 'thirty';
-  } else if (num==40) {
-    stringNum = 'forty';
-  } else if (num==50) {
-    stringNum = 'fifty';
-  } else if (num==60) {
-    stringNum = 'sixty';
-  } else if (num==70) {
-    stringNum = 'seventy';
-  } else if (num==80) {
-    stringNum = 'eighty';
-  } else if (num==90) {
-    stringNum = 'ninety';
-  } return stringNum;
-}
+  function numToString(num) {
+    let stringNum;
+    if (num==1) {
+      stringNum = 'one';
+    } else if (num==2) {
+      stringNum = 'two';
+    } else if (num==3) {
+      stringNum = 'three';
+    } else if (num==4) {
+      stringNum = 'four';
+    } else if (num==5) {
+      stringNum = 'five';
+    } else if (num==6) {
+      stringNum = 'six';
+    } else if (num==7) {
+      stringNum = 'seven';
+    } else if (num==8) {
+      stringNum = 'eight';
+    } else if (num==9) {
+      stringNum = 'nine';
+    } else if (num==0) {
+      stringNum = 'zero';
+    } else if (num==10) {
+      stringNum = 'ten';
+    } else if (num==11) {
+      stringNum = 'eleven';
+    } else if (num==12) {
+      stringNum = 'twelve';
+    } else if (num==13) {
+      stringNum = 'thirteen';
+    } else if (num==14) {
+      stringNum = 'fourteen';
+    } else if (num==15) {
+      stringNum = 'fifteen';
+    } else if (num==16) {
+      stringNum = 'sixteen';
+    } else if (num==17) {
+      stringNum = 'seventeen';
+    } else if (num==18) {
+      stringNum = 'eighteen';
+    } else if (num==19) {
+      stringNum = 'nineteen';
+    } else if (num==20) {
+      stringNum = 'twenty';
+    } else if (num==30) {
+      stringNum = 'thirty';
+    } else if (num==40) {
+      stringNum = 'forty';
+    } else if (num==50) {
+      stringNum = 'fifty';
+    } else if (num==60) {
+      stringNum = 'sixty';
+    } else if (num==70) {
+      stringNum = 'seventy';
+    } else if (num==80) {
+      stringNum = 'eighty';
+    } else if (num==90) {
+      stringNum = 'ninety';
+    } else if (num==100) {
+      stringNum = 'one hundred';
+    } else if (num==200) {
+      stringNum = 'two hundred';
+    } else if (num==300) {
+      stringNum = 'three hundred';
+    } else if (num==400) {
+      stringNum = 'four hundred';
+    } else if (num==500) {
+      stringNum = 'five hundred';
+    } else if (num==600) {
+      stringNum = 'six hundred';
+    } else if (num==700) {
+      stringNum = 'seven hundred';
+    } else if (num==800) {
+      stringNum = 'eight hundred';
+    } else if (num==900) {
+      stringNum = 'nine hundred';
+    } return stringNum;
+  }
